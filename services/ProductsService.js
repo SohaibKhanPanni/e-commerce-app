bf5284153b20bd4fc58711422ce4af31d094aa5d
sohaib_khan11 <sohaibkhan.ninesol@gmail.com>
2022-12-20 22:25:52 +0000

    commit

--- services/ProductsService.js
+++ services/ProductsService.js
@@ -4,70 +4,70 @@ const PRODUCTS = [
         name: 'Black Headset',
         price: 350,
         image: require('../assets/products/headset-100.jpg'),
-        description: 'A headset combines a headphone with microphone. Headsets are made with either a single-earpiece (mono) or a double-earpiece (mono to both ears or stereo).'
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
         id: 101,
-        name: 'FastLane Toy Car',
+        name: 'Sennheiser HD 600',
         price: 600,
-        image: require('../assets/products/car-101.jpg'),
-        description: 'A model car, or toy car, is a miniature representation of an automobile. Other miniature motor vehicles, such as trucks, buses, or even ATVs, etc. are often included in this general category.'
+        image: require('../assets/products/headset-101.jpg'),
+        description: 'Sennheiser Audiophile Range, New transducer system developed by Sennheiser, Circumaural, Open, Dynamic, Two-sided cable routing, Maximum sound pressure level: 104 dB SPL @ 1 kHz, 1V RMS, Impedance: 150 Ohm, Frequency range: 10 - 41,000 Hz, Distortion factor at 1 kHz < 0.4, Weight: 260 g.'
     },
     {
         id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        name: 'AKG K-271 MKII',
+        price: 500,
+        image: require('../assets/products/headset-102.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 103,
+        name: 'the t.bone HD 200',
+        price: 400,
+        image: require('../assets/products/headset-103.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 104,
+        name: 'beyerdynamic MMX-300 2. Generation',
+        price: 290,
+        image: require('../assets/products/headset-104.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 105,
+        name: 'AKG K-240 Studio',
+        price: 220,
+        image: require('../assets/products/headset-105.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 106,
+        name: 'Superlux HD-681',
+        price: 240,
+        image: require('../assets/products/headset-106.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 107,
+        name: 'AKG K-701',
+        price: 200,
+        image: require('../assets/products/headset-107.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 108,
+        name: 'AKG K-361',
+        price: 250,
+        image: require('../assets/products/headset-108.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     },
     {
-        id: 102,
-        name: 'SweetHome Cupcake',
-        price: 2,
-        image: require('../assets/products/cake-102.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        id: 109,
+        name: 'Dan Clark Audio EXPANSE',
+        price: 200,
+        image: require('../assets/products/headset-109.jpg'),
+        description: 'Dynamic ,Open-back ,Impedance: 300 Ohm,Sound pressure level: 103 dB,Frequency range: 10 - 41,000 Hz,3 m Cable with 6.3 mm stereo jack plug,Weight without cable: 269 g,Weight with cable: 346 g,Includes 20 cm adapter cable from 6.3 mm to 3.5 mm jack plug'
     }
 ];
 export function getProducts() {
